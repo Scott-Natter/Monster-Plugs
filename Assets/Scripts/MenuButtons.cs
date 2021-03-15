44d1c9a868f0a540b8b15694fd8bbb8e939a8106
LilAbe99 <54870102+LilAbe99@users.noreply.github.com>
2021-03-14 23:28:07 -0400

    Main menu in

---
+++ Assets/Scripts/MenuButtons.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuButtons : MonoBehaviour
+{
+    public GameObject controlsScreen;
+    public void StartGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+    public void ShowControls()
+    {
+        controlsScreen.SetActive(true);
+    }
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+    public void ReturnToMenu()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+}
