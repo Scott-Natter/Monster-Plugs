cb8f85e608427f0ab2f54a5a4d5f31d4959cdffb
LilAbe99 <54870102+LilAbe99@users.noreply.github.com>
2021-03-19 15:06:43 -0400

    Flashing red added

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.Rendering.PostProcessing;
 
 public class GameManager : MonoBehaviour
 {
@@ -15,6 +16,8 @@ public class GameManager : MonoBehaviour
     public ChargeBar overallChargeBar;
     public GameObject Monster;
     public Sprite[] MonsterSprites;
+    public Camera mainCam;
+    public float speed, minimum = 0f, maximum = 1f;
 
     public GameObject EndScreen;
     public Text SavedMonstersText;
@@ -39,6 +42,11 @@ public class GameManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if(overallChargeBar.GetComponent<Slider>().value < 25)
+        {
+            mainCam.GetComponent<PostProcessVolume>().weight = Mathf.Lerp(minimum, maximum, speed);
+            speed += 1.5f * Time.deltaTime;
+        }
         if (Timer.TimerDone == true && overallCharge > 0)
         {
             foreach (Transform child in CordHolder.transform)
@@ -57,6 +65,13 @@ public class GameManager : MonoBehaviour
             Timer.secondsLeft = 45;
             NewLevel();  
         }
+        if (speed > 1.0f)
+        {
+            float temp = maximum;
+            maximum = minimum;
+            minimum = temp;
+            speed = 0.0f;
+        }
         if (overallChargeBar.transform.GetComponent<Slider>().value <= 0)
         {
             EndScreen.SetActive(true);
@@ -68,6 +83,7 @@ public class GameManager : MonoBehaviour
     void NewLevel()
     {
         LevelNum += 1;
+        overallChargeBar.SetCharge(100);
         if (LevelNum < 3)
         {
             PlugsPerLevel = Random.Range(1,3);
